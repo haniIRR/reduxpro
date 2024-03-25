e9d23fa0cae0a039a1afab05c052c55f3261d1c1
melobit <helmaeb26@gmail.com>
2024-03-25 10:30:09 +0330

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Cart from "./components/Cart/Cart";
 import { useSelector, UseSelector } from "react-redux";
 import Layout from "./components/Layout/Layout";
@@ -6,6 +8,13 @@ import "./App.css";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    //todo
+    fetch("http", { method: "PUT", body: JSON.stringify(cart) });
+  }, [cart]);
+
   return (
     <div className="App">
       <Layout>
